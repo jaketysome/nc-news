3982cef437d261ad705a79f4ba1edb43dcf77359
Jake Tysome <jaketysome@Jakes-MacBook-Pro.local>
2023-01-11 11:27:13 +0000

    swaps postComment function for handleSubmit and changes user state to existing user

--- src/components/CommentInput.jsx
+++ src/components/CommentInput.jsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
-import { postComment } from "../utils/postComment";
+import * as api from "../utils/api";
 
 const CommentInput = ({articleId, user}) => {
     const [newComment, setNewComment] = useState();
     const { username } = user;
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        api.postCommentByArticleId(articleId, username, newComment).then((data) => {
+            console.log(data.comment, "<<<< new comment")
+        })
+    }
+
     return (
-        <form className="Comment__Form">
+        <form className="Comment__Form" onSubmit={handleSubmit}>
             <label htmlFor="comment-input"></label>
             <textarea className="Comment__Input" id="comment-input" placeholder="Enter Comment..." value={newComment} onChange={(e) => setNewComment(e.target.value)}></textarea>
             <br></br>
-            <button onClick={() => postComment(articleId, username, newComment)}>Post Comment</button>
+            <button type="submit">Post Comment</button>
         </form>
     )
 }
--- src/utils/postComment.js
+++
@@ -1,7 +0,0 @@
-import * as api from "./api";
-
-export const postComment = (articleId, username, comment) => {
-    api.postCommentByArticleId(articleId, username, comment).then((data) => {
-        console.log(data.comment, "<<<< new comment")
-    })
-}
