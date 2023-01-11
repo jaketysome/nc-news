932d429f234481e89eb30c3e12bdf48e096eed3b
Jake Tysome <jaketysome@Jakes-MacBook-Pro.local>
2023-01-11 10:34:44 +0000

    adds onChange setNewComment to textarea

--- src/components/CommentInput.jsx
+++ src/components/CommentInput.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
 const CommentInput = () => {
+    const [newComment, setNewComment] = useState();
+
+    console.log(newComment)
+
     return (
         <form className="Comment__Form">
             <label htmlFor="comment-input"></label>
-            <textarea className="Comment__Input" type="text" id="comment-input" placeholder="Enter Comment..."></textarea>
+            <textarea className="Comment__Input" id="comment-input" placeholder="Enter Comment..." value={newComment} onChange={(e) => setNewComment(e.target.value)}></textarea>
             <br></br>
             <button>Post Comment</button>
         </form>
