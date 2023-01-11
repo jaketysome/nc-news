b3a86054dc59d7f5b3ff5a46d38006991afb5b6e
Jake Tysome <jaketysome@Jakes-MacBook-Pro.local>
2023-01-11 12:01:38 +0000

    adds comment successfully posted message

--- src/components/CommentInput.jsx
+++ src/components/CommentInput.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { ClipLoader } from "react-spinners";
+import { TiTick } from "react-icons/ti";
 import * as api from "../utils/api";
 
 const CommentInput = ({ articleId, user }) => {
     const [newComment, setNewComment] = useState();
     const [postingComment, setPostingComment] = useState(false);
+    const [commentPosted, setCommentPosted] = useState(false);
     const { username } = user;
 
     const handleSubmit = (e) => {
@@ -12,6 +14,7 @@ const CommentInput = ({ articleId, user }) => {
         e.preventDefault();
         api.postCommentByArticleId(articleId, username, newComment).then((data) => {
             setPostingComment(false);
+            setCommentPosted(true);
         })
     }
 
@@ -20,9 +23,14 @@ const CommentInput = ({ articleId, user }) => {
     return (
         <form className="Comment__Form" onSubmit={handleSubmit}>
             <label htmlFor="comment-input"></label>
-            <textarea className="Comment__Input" id="comment-input" placeholder="Enter Comment..." value={newComment} onChange={(e) => setNewComment(e.target.value)}></textarea>
+            <textarea className="Comment__Input" id="comment-input" placeholder="Enter comment..." value={newComment} onChange={(e) => {
+                setCommentPosted(false);
+                setNewComment(e.target.value);
+                }}>
+            </textarea>
             <br></br>
             <button type="submit">Post Comment</button>
+            {commentPosted && <p>Comment successfully posted!! <TiTick/></p>}
         </form>
     )
 }
