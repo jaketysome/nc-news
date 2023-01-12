6a06330bf6d0e0680c60c928de71f1aa4f6c5930
Jake Tysome <jaketysome@Jakes-MacBook-Pro.local>
2023-01-12 11:54:21 +0000

    adds selected styling to HOME and TOPICS Links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import TopicsList from "./TopicsList";
 import * as api from "../utils/api";
 
 const Nav = () => {
     const [topics, setTopics] = useState();
     const [revealTopics, setRevealTopics] = useState(false);
+
+    const currURL = useLocation().pathname;
 
     useEffect(() => {
         api.getTopics().then((data) => {
@@ -16,8 +18,8 @@ const Nav = () => {
     return (
         <div>
         <nav className="Nav">
-            <Link to="/" className="Nav__link" >HOME</Link>
-            <Link className="Nav__link" onClick={() => {revealTopics ? setRevealTopics(false) : setRevealTopics(true)}}>TOPICS</Link>
+            <Link to="/" className={currURL === "/" ? "Nav__link__selected" : "Nav__link"} >HOME</Link>
+            <Link className={revealTopics ? "Nav__link__selected" : "Nav__link"} onClick={() => {revealTopics ? setRevealTopics(false) : setRevealTopics(true)}}>TOPICS</Link>
             <Link className="Nav__link" >USERS</Link>
         </nav>
         {revealTopics && <TopicsList topics={topics}/>}
