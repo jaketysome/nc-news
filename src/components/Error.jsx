4130db22dc3bcd89a81f8113ccac5129376394a3
Jake Tysome <jaketysome@Jakes-MacBook-Pro.local>
2023-01-13 12:03:04 +0000

    adds Back to salvation Link to Error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Error = ({ error, setIsHome }) => {
 
@@ -9,6 +10,7 @@ const Error = ({ error, setIsHome }) => {
     return (
         <div>
             {error !== null ? <p className="Error">{`${error.status} - ${error.data.msg}`}</p> : <p className="Error">404 - Nothing to see here!</p>}
+            <Link to="/" className="Error">Back to salvation</Link>
         </div>
     )
 }
