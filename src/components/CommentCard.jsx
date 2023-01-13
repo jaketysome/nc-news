0315b6d1c4ac0e24fb1c6e6f3558d6e56f7d2196
Jake Tysome <jaketysome@Jakes-MacBook-Pro.local>
2023-01-13 08:45:29 +0000

    adds isDeleting state and loading spinner to CommentCard

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import { formatDate } from "../utils/formatDate";
+import { ClipLoader } from "react-spinners";
 import { BiLike } from "react-icons/bi";
 import * as api from "../utils/api";
 
 const CommentCard = ({ comment, currUser, setComments, setCommentDeleted }) => {
     const [commentAuthor, setCommentAuthor] = useState();
     const [deleteError, setDeleteError] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(false);
     const { author, created_at, votes, body, comment_id } = comment;
 
     useEffect(() => {
@@ -15,15 +17,18 @@ const CommentCard = ({ comment, currUser, setComments, setCommentDeleted }) => {
     }, [author])
 
     const deleteComment = (commentId) => {
+        setIsDeleting(true);
         api.deleteCommentByCommentId(commentId).then((data) => {
             setCommentDeleted(true);
             setDeleteError(false);
+            setIsDeleting(false);
             setComments((currComments) => {
                 return currComments.filter((comment) => {
                     return comment.comment_id !== commentId;
                 })
             })
         }).catch((err) => {
+            setIsDeleting(false);
             setCommentDeleted(false);
             setDeleteError(true);
         })
@@ -37,7 +42,8 @@ const CommentCard = ({ comment, currUser, setComments, setCommentDeleted }) => {
             </div>
             <p className="Comment__Body">{body}</p>
             <p><BiLike/> {votes}</p>
-            {commentAuthor?.username === currUser.username ? <button onClick={() => {deleteComment(comment_id)}}>Delete comment</button> : null}
+            {commentAuthor?.username === currUser.username ? <button onClick={() => {deleteComment(comment_id)}}>Delete comment</button> : null}<br></br>
+            {isDeleting && <ClipLoader color="#36D7B7"/>}
             {deleteError && <p className="error-message">Oops! Comment not deleted! <br></br>Please try again...</p>}
         </div>
     )
