755af5785dca929badde995ad413b1da58cb4aa1
Jake Tysome <jaketysome@Jakes-MacBook-Pro.local>
2023-01-13 14:27:05 +0000

    removes unused setCurrUser

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ArticlesList from "./components/ArticlesList";
 import SingleArticle from "./components/SingleArticle";
 
 function App() {
-  const [currUser, setCurrUser] = useState({
+  const [currUser] = useState({
     username: "grumpy19",
     name: "Paul Grump",
     avatar_url: "https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013"
